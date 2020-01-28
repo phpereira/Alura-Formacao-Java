0479c84e6a502b3e02d552390abd11bd0c229a4e
Paulo Henrique Pereira <phpereira@outlook.com>
2020-01-28 17:22:35 -0300

    Aula 03

--- 2 - Java - Introdução a OO/bytebank/src/Conta.java
+++ 2 - Java - Introdução a OO/bytebank/src/Conta.java
@@ -3,4 +3,26 @@ class Conta {
 	int agencia;
 	int numero;
 	String titular;
+
+	void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
 }
