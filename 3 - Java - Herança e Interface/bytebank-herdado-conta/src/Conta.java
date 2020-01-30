8d51cbc2222bd0ff57175c96cad60e224542deb5
Paulo Henrique Pereira <phpereira@outlook.com>
2020-01-30 17:22:34 -0300

    Aula 6 - Interfaces

--- 3 - Java - Herança e Interface/bytebank-herdado-conta/src/Conta.java
+++ 3 - Java - Herança e Interface/bytebank-herdado-conta/src/Conta.java
@@ -1,4 +1,5 @@
 public abstract class Conta {
+	
 	private double saldo;
 	private int agencia;
 	private int numero;
--- 3 - Java - Herança e Interface/bytebank-herdado/src/Gerente.java
+++ 3 - Java - Herança e Interface/bytebank-herdado/src/Gerente.java
@@ -1,24 +1,25 @@
-//Gerente herda(extends) Funcionario
-public class Gerente extends Funcionario{
+//Gerente herda(extends) Funcionario e assina o contrato autenticavel
+public class Gerente extends Funcionario implements Autenticavel {
+
 	private int senha;
-	
-	
+
+	public double getBonificacao() {
+		System.out.println("Testa bonificação do Gerente");
+		return super.getSalario(); // super diz que a salario está definido na classe pai funcionario
+	}
+
+	@Override
 	public void setSenha(int senha) {
 		this.senha = senha;
 	}
-	
 
+	@Override
 	public boolean autentica(int senha) {
-		if(this.senha == senha) {
+		if (this.senha == senha) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	public double getBonificacao() {
-		System.out.println("Testa bonificação do Gerente");
-		return super.getSalario(); //super diz que a salario está definido na classe pai funcionario
-	}
-	
+
 }
